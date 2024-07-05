9167888ecb29798df82708b00467088d17d3214a
Lil-Young <thddyddls1@naver.com>
2024-07-05 22:25:06 +0900

    update

---
+++ java_pratice/src/pratice/array_ex/ArrayEx1.java
@@ -0,0 +1,13 @@
+package pratice.array_ex;
+
+public class ArrayEx1 {
+	// 다음 문제를 배열을 사용해서 개선하자
+	// 학생5명의 점수가 90, 80, 70, 60, 50이 각각 변수로 있다. 
+	// 전체 학생 점수의 합과 평균을 출력하라
+	
+
+	int[] students = new int[] {90, 80, 70, 60, 50};
+	for(int num : students) {
+		System.out.println(num);
+	}
+}
